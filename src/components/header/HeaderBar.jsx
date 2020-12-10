896ef6784e311daced4908c260a13d97f883f5d6
FacuBenito <facuben1997@gmail.com>
2020-12-10 19:38:10 -0300

    fixed a bug in which sometimes products would load but you'd get the error screen

--- src/components/header/HeaderBar.jsx
+++ src/components/header/HeaderBar.jsx
@@ -27,7 +27,9 @@ const HeaderBar = ({showModal}) => {
 	}
 
 	useEffect(() => {
-		getHistory();
+		if(showHistory){
+			getHistory();
+		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	},[showHistory]);
 
